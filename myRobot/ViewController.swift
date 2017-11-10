b4254f5f8be5bf3d68fce4109c487bee5c3ba08b
alictronix <armjaz@gmail.com>
2017-11-10 20:01:54 +0000

    first commit

--- myRobot/ViewController.swift
+++ myRobot/ViewController.swift
@@ -19,7 +19,100 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var web: UIWebView!
+    var ipAdd: String = ""
+    var url: String = ""
+    var urlF: String = ""
+    var urlB: String = ""
+    var urlR: String = ""
+    var urlL: String = ""
+    var urlS: String = ""
 
+    @IBOutlet weak var label: UILabel!
 
+    @IBOutlet weak var input: UITextField!
+    @IBAction func setIP(_ sender: Any) {
+        ipAdd=input.text!
+        label.text=ipAdd
+        
+        url="http://"
+        url+=ipAdd
+        
+        urlS+=url
+        urlS+="/key/1"
+        
+        urlF+=url
+        urlF+="/key/2"
+        
+        urlR += url
+        urlR += "/key/4"
+        
+        urlL += url
+        urlL += "/key/5"
+        
+        urlB += url
+        urlB += "/key/3"
+        
+    }
+    
+    
+    @IBOutlet weak var temp: UILabel!
+    
+    
+    @IBAction func Forward(_ sender: Any) {
+        temp.text=urlF
+        let requestURL = URL(string: urlF)
+        let request = URLRequest(url: requestURL!)
+        web.loadRequest(request)
+    }
+    
+    @IBAction func Forwardx(_ sender: Any) {
+        temp.text=urlS
+        let requestURL = URL(string: urlS)
+        let request = URLRequest(url: requestURL!)
+        web.loadRequest(request)
+    }
+    @IBAction func Right(_ sender: Any) {
+        
+        temp.text=urlR
+        let requestURL = URL(string: urlR)
+        let request = URLRequest(url: requestURL!)
+        web.loadRequest(request)
+    }
+    
+    @IBAction func Rightx(_ sender: Any) {
+        temp.text=urlS
+        let requestURL = URL(string: urlS)
+        let request = URLRequest(url: requestURL!)
+        web.loadRequest(request)
+    }
+    
+    @IBAction func Left(_ sender: Any) {
+        temp.text=urlL
+        let requestURL = URL(string: urlL)
+        let request = URLRequest(url: requestURL!)
+        web.loadRequest(request)
+    }
+    
+    @IBAction func Leftx(_ sender: Any) {
+        temp.text=urlS
+        let requestURL = URL(string: urlS)
+        let request = URLRequest(url: requestURL!)
+        web.loadRequest(request)
+    }
+    @IBAction func Back(_ sender: Any) {
+        temp.text=urlB
+        let requestURL = URL(string: urlB)
+        let request = URLRequest(url: requestURL!)
+        web.loadRequest(request)
+    }
+    
+    @IBAction func Backx(_ sender: Any) {
+        temp.text=urlS
+        let requestURL = URL(string: urlS)
+        let request = URLRequest(url: requestURL!)
+        web.loadRequest(request)
+    }
 }
 
